3b97e2c61b6299cfb9dd41bc01595c9a555114f1
rickosong <ricko.aprilianto00@gmail.com>
2023-07-15 11:36:50 +0800

    bikin register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Helper\ApiFormatter;
 
 class AuthController extends Controller
 {
-    //
+    function RegisterUser(Request $request){
+        $credentials = $request->validate( [
+            'users' => 'required|string',
+            'email' => 'required|email:dns|min:3|max:255|unique:users|confirmed',
+            'password' => 'required|min:5|max:255',
+        ]);
+
+        $user = New User();
+            $user->email = $request->email;
+            $user->name = $request->nama;
+            $user->password = bcrypt($request->password);
+            $user->phonenumber = '62' . $request->phone;
+            $user->birth = 'Silahkan isi tanggal lahir anda';
+            $user->image = 'user.svg';
+            $user->havebengkel = false;
+
+            if($user->save()){
+                $token = $user->createToken('token')->plainTextToken;
+                return ApiFormatter::createApi(200, 'register berhasil', $token);
+            } else{
+                return ApiFormatter::createApi(401, 'register gagal');
+            }
+
+
+        
+    }
 }
