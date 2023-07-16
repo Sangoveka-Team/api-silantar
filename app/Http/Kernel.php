d3963caaecb5fd7fcf8e34cadff181004d84efd5
rickosong <ricko.aprilianto00@gmail.com>
2023-07-17 02:19:35 +0800

    membuat middleware untuk para user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,10 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'pelapor' => \App\Http\Middleware\Pelapor::class,
+        'lurah' => \App\Http\Middleware\Lurah::class,
+        'dinas' => \App\Http\Middleware\Dinas::class,
+        'superadmin' => \App\Http\Middleware\Superadmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
