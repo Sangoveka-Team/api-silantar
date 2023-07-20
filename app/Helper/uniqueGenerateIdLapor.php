df2c65ebc1ac7d8ba0b4e4d225933ac940c21133
rickosong <ricko.aprilianto00@gmail.com>
2023-07-21 01:14:45 +0800

    method untuk generate id laporan

--- app/Helper/uniqueGenerateIdLapor.php
+++ app/Helper/uniqueGenerateIdLapor.php
@@ -1,8 +1,19 @@
 <?php
 
-    Namespace App\Helper;
+Namespace App\Helper;
+use Illuminate\Support\Str;
 
-    Class uniqueGenerateIdLapor{
+Class uniqueGenerateIdLapor
+{
+    public static function generateUniqueRandomString($length, $model, $field)
+    {
+        $randomString = Str::random($length);
 
+        while ($model->where($field, $randomString)->exists()) {
+            $randomString = Str::random($length);
+        }
+
+        return $randomString;
     }
+}
 ?>
