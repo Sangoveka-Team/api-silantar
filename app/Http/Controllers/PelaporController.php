8ad11d8c4d28ebb3d39619114f1fa206b7e22d46
rickosong <ricko.aprilianto00@gmail.com>
2023-07-20 11:22:40 +0800

    membuat read profil dan update profil

--- app/Http/Controllers/PelaporController.php
+++ app/Http/Controllers/PelaporController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\ApiFormatter;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class PelaporController extends Controller
 {
-    //
+    public function profile(){
+        try {
+            $user = User::find(auth()->user()->id);
+
+            return ApiFormatter::createApi(200, 'success', $user);
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(401, 'failed', $error);
+        }
+    }
+
+
+    public function updateProfile(Request $request){
+        try {
+            $user = User::findOrFail(auth()->user()->id);
+
+            $user->nama = $request->nama;
+            $user->nomor = $request->nomor;
+            // $user->email = $request->email;
+            $user->daerah = "-";
+            $user->password = bcrypt($request->password);
+
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $imageName = time() . '.' . $image->extension();
+                $image->move(public_path('img'), $imageName);
+                
+                $user->image = $imageName;
+            } else {
+                $user->image = $user->image;
+            }
+
+            // dd($request->nama);
+
+            if ($user->update()) {
+                return ApiFormatter::createApi(200, 'success', $user);
+            } else{
+            return ApiFormatter::createApi(401, 'failed');
+            }
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(401, 'failed', $error);
+        }
+    }
 }
