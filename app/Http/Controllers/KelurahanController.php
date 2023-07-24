3483478542089e27875b34b2a56b737bf073df91
rickosong <ricko.aprilianto00@gmail.com>
2023-07-25 00:56:20 +0800

    buat fitur lihat profil dan update profil

--- app/Http/Controllers/KelurahanController.php
+++ app/Http/Controllers/KelurahanController.php
@@ -3,6 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Helper\ApiFormatter;
+use App\Helper\uniqueGenerateIdLapor;
+use App\Models\Laporan;
+use Exception;
+use App\Models\Images;
+use App\Models\Kategori;
+use App\Models\Status;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
 
 class KelurahanController extends Controller
 {
@@ -11,23 +22,92 @@ class KelurahanController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $allLaporan = Laporan::where('daerah_kelurahan', auth()->user()->id)->get();
+            $allLaporanBelumDiproses = Laporan::where('daerah_kelurahan', auth()->user()->id)->where('status_lapor', 1)->get();
+            $allLaporanDiproses = Laporan::where('daerah_kelurahan', auth()->user()->id)->where('status_lapor', 2)->get();
+            $allLaporanDitolak = Laporan::where('daerah_kelurahan', auth()->user()->id)->where('status_lapor', 3)->get();
+            $allLaporanTuntas = Laporan::where('daerah_kelurahan', auth()->user()->id)->where('status_lapor', 4)->get();
+
+            $laporanCount = $allLaporan->count();
+            $laporanBelumDiprosesCount = $allLaporanBelumDiproses->count();
+            $laporaniprosesCount = $allLaporanDiproses->count();
+            $laporanDitolakCount = $allLaporanDitolak->count();
+            $laporanTuntasCount = $allLaporanTuntas->count();
+
+            $namaUser = auth()->user()->nama;
+
+
+            $data = [
+                "allLaporan" => $laporanCount,
+                "laporanBelumDiproses" => $laporanBelumDiprosesCount,
+                "laporanDiproses" => $laporaniprosesCount,
+                "laporanDitolak" => $laporanDitolakCount,
+                "laporanTuntas" => $laporanTuntasCount,
+                "namaAdminKelurahan" => $namaUser,
+                "laporanLurah" => $allLaporan,
+            ];
+
+            return ApiFormatter::createApi(200, 'success', $data);
+
+            
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(401, 'error', $error);
+        }
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function profile()
     {
-        //
+        try {
+            $user = User::find(auth()->user()->id);
+
+            return ApiFormatter::createApi(200, 'success', $user);
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(401, 'failed', $error);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function updateProfile(Request $request)
     {
-        //
+        try {
+            $user = User::findOrFail(auth()->user()->id);
+
+            $user->nama = $request->nama;
+            $user->nomor = $request->nomor;
+            // $user->email = $request->email;
+            // $user->daerah = $request->daerah;
+            $user->jabatan = $request->jabatan;
+            $user->password = bcrypt($request->password);
+
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $imageName = time() . '.' . $image->extension();
+                $image->move(public_path('img'), $imageName);
+                $path =  "public/img/" . $imageName;
+                
+                $user->image = $path;
+            } else {
+                $user->image = $user->image;
+            }
+
+            // dd($request->nama);
+
+            if ($user->update()) {
+                return ApiFormatter::createApi(200, 'success', $user);
+            } else{
+            return ApiFormatter::createApi(401, 'failed');
+            }
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(401, 'failed', $error);
+        }
     }
 
     /**
