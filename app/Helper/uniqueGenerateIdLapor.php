4097f5ef9c701d108d0285b095bb9986c30a8832
rickosong <ricko.aprilianto00@gmail.com>
2023-07-28 23:40:03 +0800

    perbaikan pada generate string

--- app/Helper/uniqueGenerateIdLapor.php
+++ app/Helper/uniqueGenerateIdLapor.php
@@ -8,10 +8,22 @@ Class uniqueGenerateIdLapor
 {
     public static function generateUniqueRandomString($length, $model, $field)
     {
-        $randomString = Str::random($length);
+        $randomString = "";
+        $characters = '123456789';
+        $max        = strlen($characters) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $rand = mt_rand(0, $max);
+            $randomString .= $characters[$rand];
+        }
 
         while (Laporan::where($field, $randomString)->exists()) {
-            $randomString = Str::random($length);
+            $randomString = "";
+            $characters = '123456789';
+            $max        = strlen($characters) - 1;
+            for ($i = 0; $i < $length; $i++) {
+                $rand = mt_rand(0, $max);
+                $randomString .= $characters[$rand];
+            }
         }
 
         return $randomString;
